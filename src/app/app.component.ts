5e8f042b1bc9c3ca21173ab800e9f75c76ad38bf
Padraic O'Neill <pconeill@gmail.com>
2024-04-19 13:10:08 -0600

    prosemirror

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,51 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import * as Y from 'yjs'
-import { WebrtcProvider } from 'y-webrtc'
-import Quill from 'quill';
-import { QuillBinding } from 'y-quill'
-import QuillCursors from 'quill-cursors';
+import { NgxEditorModule } from 'ngx-editor';
+import { Editor } from 'ngx-editor';
+import { FormsModule } from '@angular/forms';
 
-Quill.register('modules/cursors', QuillCursors as any)
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NgxEditorModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-  title = 'quill-collaborative';
-  quill!: Quill;
-  ydoc: Y.Doc;
-  provider: WebrtcProvider;
-  yarray: Y.Array<any>;
 
-  constructor() {
-    this.ydoc = new Y.Doc();
-    // clients connected to the same room-name share document updates
-    this.provider = new WebrtcProvider('your-room-name', this.ydoc, { password: 'optional-room-password' })
-    this.yarray = this.ydoc.get('array', Y.Array)
+export class AppComponent implements OnInit, OnDestroy {
+  title = 'prosemirror-collaborative';
+  editor!: Editor;
+  html = '';
+  
+  ngOnInit(): void {
+    this.editor = new Editor();
   }
 
-  ngOnInit(): void {
-    const type = this.ydoc.getText('quill')
-
-    var editor = new Quill('#editor', {
-      modules: {
-        cursors: true,
-        toolbar: [
-          [{ header: [1, 2, false] }],
-          ['bold', 'italic', 'underline'],
-          ['image', 'code-block']
-        ]
-      },
-      placeholder: 'Start collaborating...',
-      theme: 'snow' // or 'bubble'
-    })
-
-    // Optionally specify an Awareness instance, if supported by the Provider
-    const binding = new QuillBinding(type, editor, this.provider.awareness)
+  // make sure to destory the editor
+  ngOnDestroy(): void {
+    this.editor.destroy();
   }
 }
