72b09a1c4a1267135b040c97e06d1064e2ff1532
Padraic O'Neill <pconeill@gmail.com>
2024-04-26 11:44:10 -0600

    adds full featured toolbar

--- src/app/editor/editor/editor.component.ts
+++ src/app/editor/editor/editor.component.ts
@@ -36,6 +36,17 @@ export class EditorComponent implements OnInit, OnDestroy {
     editorContent: new FormControl(""),
   });
 
+  toolbar: Toolbar = [
+    ['bold', 'italic'],
+    ['underline', 'strike'],
+    ['code', 'blockquote'],
+    ['ordered_list', 'bullet_list'],
+    [{ heading: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] }],
+    ['link', 'image'],
+    ['text_color', 'background_color'],
+    ['align_left', 'align_center', 'align_right', 'align_justify'],
+  ];
+
   googleSearchPlugin = new Plugin({
     key: new PluginKey("selection-google"),
     view: () => ({
